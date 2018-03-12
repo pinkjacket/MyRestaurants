28ad6e0f2991bff1d6fe5ba2471572c3cc97d780
James Donlan <jacketlizard@live.com>
2018-03-12 16:01:23 -0700

    added fonts

--- app/src/main/java/pinkjacket/myrestaurants/MainActivity.java
+++ app/src/main/java/pinkjacket/myrestaurants/MainActivity.java
@@ -1,18 +1,21 @@
 package pinkjacket.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mLocationEditText =(EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface robotoFont = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+        mAppNameTextView.setTypeface(robotoFont);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
