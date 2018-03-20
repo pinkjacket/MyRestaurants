765b13a5bd293be1fb4665dfa81f28eb53cb36a7
Student <example@example.com>
2018-03-20 16:02:19 -0700

    recyclerview implemented

--- app/src/main/java/pinkjacket/myrestaurants/ui/RestaurantsActivity.java
+++ app/src/main/java/pinkjacket/myrestaurants/ui/RestaurantsActivity.java
@@ -3,6 +3,8 @@ package pinkjacket.myrestaurants.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,12 +22,13 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 import pinkjacket.myrestaurants.R;
+import pinkjacket.myrestaurants.adapters.RestaurantListAdapter;
 import pinkjacket.myrestaurants.models.Restaurant;
 import pinkjacket.myrestaurants.services.YelpService;
 
 public class RestaurantsActivity extends AppCompatActivity {
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
 //    private String[] restaurants = new String[] {"Over the Moon", "Cafe Nervosa", "Bel Canto", "Jean's", "Cucina", "Tony's Meatball Hutch", "Shake Shack", "Lucky & Son", "Luscious Grape", "Uncle Moe's Family Feedbag", "Los Pollos Hermanos", "Boar's Nest", "Snowclone Coffee", "Cheers", "Legitimate Bakery", "Soup and a Sandwich", "Slammin' Nutt", "Pastabilities"};
 //    private String[] cuisines = new String[] {"Charcuterie", "Coffee and pastries", "Italian food", "Coffee and snacks", "Italian food", "Meatballs", "Burgers and shakes", "Breakfast", "Wine", "Family-style dining", "Chicken", "Delicatessen", "Coffee", "Beer", "Baked goods", "Soup and sandwiches", "Donuts", "Pasta"};
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
@@ -38,20 +41,8 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
-
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
         getRestaurants(location);
     }
     private void getRestaurants(String location){
@@ -70,24 +61,23 @@ public class RestaurantsActivity extends AppCompatActivity {
 
                     @Override
                     public void run(){
-                        String[] restaurantNames = new String[restaurants.size()];
-                        for (int i = 0; i < restaurantNames.length; i++){
-                            restaurantNames[i] = restaurants.get(i).getName();
-                        }
-                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this,
-                                android.R.layout.simple_list_item_1, restaurantNames);
-                        mListView.setAdapter(adapter);
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), restaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(RestaurantsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
 
-                        for (Restaurant restaurant : restaurants) {
-                            Log.d(TAG, "Name: " + restaurant.getName());
-                            Log.d(TAG, "Phone: " + restaurant.getPhone());
-                            Log.d(TAG, "Website: " + restaurant.getWebsite());
-                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
+                        //old code to log api results, not needed now but I don't want to get rid of it yet
+//                        for (Restaurant restaurant : restaurants) {
+//                            Log.d(TAG, "Name: " + restaurant.getName());
+//                            Log.d(TAG, "Phone: " + restaurant.getPhone());
+//                            Log.d(TAG, "Website: " + restaurant.getWebsite());
+//                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
+//                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
+//                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
+//                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
                         }
-                    }
                 });
             }
         });
