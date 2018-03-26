bf847d1642ae4cc8c77cdc7e91d2f1967aa54738
Student <example@example.com>
2018-03-26 13:53:35 -0700

    can now save restaurants to their own list

--- app/src/main/java/pinkjacket/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/pinkjacket/myrestaurants/ui/RestaurantDetailFragment.java
@@ -10,13 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import pinkjacket.myrestaurants.Constants;
 import pinkjacket.myrestaurants.R;
 import pinkjacket.myrestaurants.models.Restaurant;
 
@@ -68,6 +72,7 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
         mWebsiteLabel.setOnClickListener(this);
         mPhoneLabel.setOnClickListener(this);
         mAddressLabel.setOnClickListener(this);
+        mSaveRestaurantButton.setOnClickListener(this);
 
         return view;
     }
@@ -90,6 +95,13 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
                     + "," + mRestaurant.getLongitude()
                     + "?q=(" + mRestaurant.getName() + ")"));
             startActivity(mapIntent);
+        }
+        if(v == mSaveRestaurantButton){
+            DatabaseReference restaurantRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
+            restaurantRef.push().setValue(mRestaurant);
+            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();97201
         }
     }
 
